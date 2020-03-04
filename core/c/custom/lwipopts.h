9846ca2a215e1969a531311b996cb5bbcc01739a
Syrone Wong <wong.syrone@gmail.com>
2020-03-04 13:25:25 +0800

    lwip: enlarge tcp snd buf
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- core/c/custom/lwipopts.h
+++ core/c/custom/lwipopts.h
@@ -67,8 +67,7 @@
 #define MEMP_NUM_TCP_SEG 128
 
 #define TCP_MSS 1460
-#define TCP_SND_BUF 16384
-#define TCP_SND_QUEUELEN (4 * (TCP_SND_BUF)/(TCP_MSS))
+#define TCP_SND_BUF (65535)
 
 #define LWIP_TCP_SACK_OUT 1
 #define LWIP_TCP_KEEPALIVE 1
