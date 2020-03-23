c7614f9e1b619a3da38f7fa14a6eef89728b2d6a
Syrone Wong <wong.syrone@gmail.com>
2020-03-23 23:58:10 +0800

    Add mutex debugging facility
    disabled by default, please uncomment to start debugging
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- core/lwip_access_wrapper.go
+++ core/lwip_access_wrapper.go
@@ -30,34 +30,46 @@ type MutexWrapper struct {
 	count int32
 }
 
-func (m *MutexWrapper) Lock() {
-	/*
-		pc := make([]uintptr, 10) // at least 1 entry needed
-		runtime.Callers(2, pc)
-		f := runtime.FuncForPC(pc[0])
-		file, line := f.FileLine(pc[0])
-		s := fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
-	*/
+// pass anything to disable log trace
+// pass nothing to enable log trace
+func (m *MutexWrapper) Lock(/*params ...bool*/) {
+	/*if len(params) != 0 {
+		m.lock.Lock()
+		atomic.AddInt32(&m.count, 1)
+		return
+	}
 
-	//log.Infof("MutexWrapper before Lock %v %s", atomic.LoadInt32(&m.count), s)
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	s := fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
+
+	log.Infof("MutexWrapper before Lock %v %s", atomic.LoadInt32(&m.count), s) */
 	m.lock.Lock()
 	atomic.AddInt32(&m.count, 1)
-	//log.Infof("MutexWrapper after Lock %v %s", atomic.LoadInt32(&m.count), s)
+	/*log.Infof("MutexWrapper after Lock %v %s", atomic.LoadInt32(&m.count), s) */
 }
 
-func (m *MutexWrapper) Unlock() {
-	/*
-		pc := make([]uintptr, 10) // at least 1 entry needed
-		runtime.Callers(2, pc)
-		f := runtime.FuncForPC(pc[0])
-		file, line := f.FileLine(pc[0])
-		s := fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
-	*/
+// pass anything to disable log trace
+// pass nothing to enable log trace
+func (m *MutexWrapper) Unlock(/*params ...bool*/) {
+	/*if len(params) != 0 {
+		m.lock.Unlock()
+		atomic.AddInt32(&m.count, -1)
+		return
+	}
 
-	//log.Infof("MutexWrapper before Unlock %v %s", atomic.LoadInt32(&m.count), s)
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	s := fmt.Sprintf("%s:%d %s\n", file, line, f.Name())
+
+	log.Infof("MutexWrapper before Unlock %v %s", atomic.LoadInt32(&m.count), s) */
 	m.lock.Unlock()
 	atomic.AddInt32(&m.count, -1)
-	//log.Infof("MutexWrapper after Unlock %v %s", atomic.LoadInt32(&m.count), s)
+	/*log.Infof("MutexWrapper after Unlock %v %s", atomic.LoadInt32(&m.count), s) */
 }
 
 // ipaddr_ntoa() is using a global static buffer to return result,
